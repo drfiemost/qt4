drfiemost/qt4

commit 168bb2b650e91cc5cc81eef1dc23a9972472f211

Apply changes from upstream
https://github.com/qt/qtbase/commit/a5233b6b22e20bf249d8ae993a4224c6874abccf
https://github.com/qt/qtbase/commit/4c8a4058c359c8d163c643120426079fc80c8214
https://github.com/qt/qtbase/commit/9c1680a7eda7f465d1db0fe1298d7aabd744bd15

src/corelib/tools/qbytearray.cpp
@@ -57,7 +57,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define IS_RAW_DATA(d) ((d)->offset != 0)
+#define IS_RAW_DATA(d) ((d)->offset != sizeof(QByteArrayData))
 
 QT_BEGIN_NAMESPACE
 
@@ -557,7 +557,7 @@ QByteArray qUncompress(const uchar* data, int nbytes)
         }
         d.take(); // realloc was successful
         d.reset(p);
-        d->offset = 0;
+        d->offset = sizeof(QByteArrayData);
 
         int res = ::uncompress((uchar*)d->data(), &len,
                                (uchar*)data+4, nbytes-4);
@@ -583,7 +583,7 @@ QByteArray qUncompress(const uchar* data, int nbytes)
             d->size = len;
             d->alloc = len;
             d->capacityReserved = false;
-            d->offset = 0;
+            d->offset = sizeof(QByteArrayData);
             d->data()[len] = 0;
 
             return QByteArray(d.take(), 0, 0);
@@ -618,9 +618,9 @@ static inline char qToLower(char c)
 }
 
 const QStaticByteArrayData<1> QByteArray::shared_null = { { Q_REFCOUNT_INITIALIZE_STATIC,
-                                                           0, 0, 0, { 0 } }, { 0 } };
+                                                           0, 0, 0, sizeof(QByteArrayData) }, { 0 } };
 const QStaticByteArrayData<1> QByteArray::shared_empty = { { Q_REFCOUNT_INITIALIZE_STATIC,
-                                                            0, 0, 0, { 0 } }, { 0 } };
+                                                            0, 0, 0, sizeof(QByteArrayData) }, { 0 } };
 
 /*!
     \class QByteArray
@@ -1311,7 +1311,7 @@ QByteArray::QByteArray(const char *str)
         d->size = len;
         d->alloc = len;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QByteArrayData);
         memcpy(d->data(), str, len+1); // include null terminator
     }
 }
@@ -1340,7 +1340,7 @@ QByteArray::QByteArray(const char *data, int size)
         d->size = size;
         d->alloc = size;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QByteArrayData);
         memcpy(d->data(), data, size);
         d->data()[size] = '\0';
     }
@@ -1364,7 +1364,7 @@ QByteArray::QByteArray(int size, char ch)
         d->size = size;
         d->alloc = size;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QByteArrayData);
         memset(d->data(), ch, size);
         d->data()[size] = '\0';
     }
@@ -1384,7 +1384,7 @@ QByteArray::QByteArray(int size, Qt::Initialization)
     d->size = size;
     d->alloc = size;
     d->capacityReserved = false;
-    d->offset = 0;
+    d->offset = sizeof(QByteArrayData);
     d->data()[size] = '\0';
 }
 
@@ -1406,7 +1406,7 @@ void QByteArray::resize(int size)
     if (size < 0)
         size = 0;
 
-    if (d->offset && !d->ref.isShared() && size < d->size) {
+    if (IS_RAW_DATA(d) && !d->ref.isShared() && size < d->size) {
         d->size = size;
         return;
     }
@@ -1465,14 +1465,14 @@ QByteArray &QByteArray::fill(char ch, int size)
 
 void QByteArray::realloc(int alloc)
 {
-    if (d->ref.isShared() || d->offset) {
-        Data *x = static_cast<Data *>(qMalloc(sizeof(Data) + alloc + 1));
+    if (d->ref.isShared() || IS_RAW_DATA(d)) {
+        Data *x = static_cast<Data *>(malloc(sizeof(Data) + alloc + 1));
         Q_CHECK_PTR(x);
         x->ref.initializeOwned();
         x->size = qMin(alloc, d->size);
         x->alloc = alloc;
         x->capacityReserved = d->capacityReserved;
-        x->offset = 0;
+        x->offset = sizeof(QByteArrayData);
         ::memcpy(x->data(), d->data(), x->size);
         x->data()[x->size] = '\0';
         if (!d->ref.deref())
@@ -1482,7 +1482,7 @@ void QByteArray::realloc(int alloc)
         Data *x = static_cast<Data *>(::realloc(d, sizeof(Data) + alloc + 1));
         Q_CHECK_PTR(x);
         x->alloc = alloc;
-        x->offset = 0;
+        x->offset = sizeof(QByteArrayData);
         d = x;
     }
 }
@@ -1504,7 +1504,7 @@ void QByteArray::expand(int i)
 QByteArray QByteArray::nulTerminated() const
 {
     // is this fromRawData?
-    if (d->offset)
+    if (!IS_RAW_DATA(d))
         return *this;           // no, then we're sure we're zero terminated
 
     QByteArray copy(*this);
@@ -3893,7 +3893,7 @@ QByteArray QByteArray::fromRawData(const char *data, int size)
         x->size = size;
         x->alloc = 0;
         x->capacityReserved = false;
-        x->offset = data - (x->d + sizeof(qptrdiff));
+        x->offset = data - reinterpret_cast<char *>(x);
     }
     return QByteArray(x, 0, 0);
 }
@@ -3919,9 +3919,9 @@ QByteArray &QByteArray::setRawData(const char *data, uint size)
     } else {
         if (data) {
             d->size = size;
-            d->offset = data - (d->d + sizeof(qptrdiff));
+            d->offset = data - reinterpret_cast<char *>(d);
         } else {
-            d->offset = 0;
+            d->offset = sizeof(QByteArrayData);
             d->size = 0;
             *d->data() = 0;
         }

src/corelib/tools/qbytearray.h
@@ -138,12 +138,11 @@ struct QByteArrayData
     int size;
     uint alloc : 31;
     uint capacityReserved : 1;
-    union {
-        qptrdiff offset; // will always work as we add/subtract from a ushort ptr
-        char d[sizeof(qptrdiff)];
-    };
-    inline char *data() { return d + sizeof(qptrdiff) + offset; }
-    inline const char *data() const { return d + sizeof(qptrdiff) + offset; }
+
+    qptrdiff offset;
+
+    inline char *data() { return reinterpret_cast<char *>(this) + offset; }
+    inline const char *data() const { return reinterpret_cast<const char *>(this) + offset; }
 };
 
 template<int N> struct QStaticByteArrayData
@@ -162,7 +161,7 @@ template<int N> struct QStaticByteArrayDataPtr
 #  define QByteArrayLiteral(str) ([]() -> QStaticByteArrayDataPtr<sizeof(str) - 1> { \
         enum { Size = sizeof(str) - 1 }; \
         static const QStaticByteArrayData<Size> qbytearray_literal = \
-        { { Q_REFCOUNT_INITIALIZE_STATIC, Size, 0, 0, { 0 } }, str }; \
+        { { Q_REFCOUNT_INITIALIZE_STATIC, Size, 0, 0, sizeof(QByteArrayData) }, str }; \
         QStaticByteArrayDataPtr<Size> holder = { &qbytearray_literal }; \
     return holder; }())
 
@@ -175,7 +174,7 @@ template<int N> struct QStaticByteArrayDataPtr
     __extension__ ({ \
         enum { Size = sizeof(str) - 1 }; \
         static const QStaticByteArrayData<Size> qbytearray_literal = \
-        { { Q_REFCOUNT_INITIALIZE_STATIC, Size, 0, 0, { 0 } }, str }; \
+        { { Q_REFCOUNT_INITIALIZE_STATIC, Size, 0, 0, sizeof(QByteArrayData) }, str }; \
         QStaticByteArrayDataPtr<Size> holder = { &qbytearray_literal }; \
         holder; })
 #endif
@@ -399,7 +398,7 @@ public:
 
     template <int n>
     inline QByteArray(const QStaticByteArrayData<n> &dd)
-        : d(const_cast<QByteArrayData *>(&dd.str)) {}
+        : d(const_cast<QByteArrayData *>(&dd.ba)) {}
     template <int N>
     Q_DECL_CONSTEXPR inline QByteArray(QStaticByteArrayDataPtr<N> dd)
         : d(const_cast<QByteArrayData *>(&dd.ptr->ba)) {}
@@ -472,7 +471,7 @@ inline const char *QByteArray::data() const
 inline const char *QByteArray::constData() const
 { return d->data(); }
 inline void QByteArray::detach()
-{ if (d->ref.isShared() || d->offset) realloc(d->size); }
+{ if (d->ref.isShared() || (d->offset != sizeof(QByteArrayData))) realloc(d->size); }
 inline bool QByteArray::isDetached() const
 { return !d->ref.isShared(); }
 inline QByteArray::QByteArray(const QByteArray &a) : d(a.d)

src/corelib/tools/qstring.cpp
@@ -97,6 +97,8 @@
 #define ULLONG_MAX quint64_C(18446744073709551615)
 #endif
 
+#define IS_RAW_DATA(d) ((d)->offset != sizeof(QStringData))
+
 QT_BEGIN_NAMESPACE
 
 #ifdef QT3_SUPPORT
@@ -794,8 +796,8 @@ const QString::Null QString::null = { };
     \sa split()
 */
 
-const QStaticStringData<1> QString::shared_null = { { Q_REFCOUNT_INITIALIZE_STATIC, 0, 0, false, { 0 } }, { 0 } };
-const QStaticStringData<1> QString::shared_empty = { { Q_REFCOUNT_INITIALIZE_STATIC, 0, 0, false, { 0 } }, { 0 } };
+const QStaticStringData<1> QString::shared_null = { { Q_REFCOUNT_INITIALIZE_STATIC, 0, 0, false, sizeof(QStringData) }, { 0 } };
+const QStaticStringData<1> QString::shared_empty = { { Q_REFCOUNT_INITIALIZE_STATIC, 0, 0, false, sizeof(QStringData) }, { 0 } };
 
 int QString::grow(int size)
 {
@@ -1052,7 +1054,7 @@ QString::QString(const QChar *unicode, int size)
         d->size = size;
         d->alloc = (uint) size;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QStringData);
         memcpy(d->data(), unicode, size * sizeof(QChar));
         d->data()[size] = '\0';
     }
@@ -1084,7 +1086,7 @@ QString::QString(const QChar *unicode)
              d->size = size;
              d->alloc = (uint) size;
              d->capacityReserved = false;
-             d->offset = 0;
+             d->offset = sizeof(QStringData);
              memcpy(d->data(), unicode, size * sizeof(QChar));
              d->data()[size] = '\0';
          }
@@ -1109,7 +1111,7 @@ QString::QString(int size, QChar ch)
         d->size = size;
         d->alloc = (uint) size;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QStringData);
         d->data()[size] = '\0';
         ushort *i = d->data() + size;
         ushort *b = d->data();
@@ -1133,7 +1135,7 @@ QString::QString(int size, Qt::Initialization)
     d->size = size;
     d->alloc = (uint) size;
     d->capacityReserved = false;
-    d->offset = 0;
+    d->offset = sizeof(QStringData);
     d->data()[size] = '\0';
 }
 
@@ -1155,7 +1157,7 @@ QString::QString(QChar ch)
     d->size = 1;
     d->alloc = 1;
     d->capacityReserved = false;
-    d->offset = 0;
+    d->offset = sizeof(QStringData);
     d->data()[0] = ch.unicode();
     d->data()[1] = '\0';
 }
@@ -1260,7 +1262,7 @@ void QString::resize(int size)
     if (size < 0)
         size = 0;
 
-    if (d->offset && !d->ref.isShared() && size < d->size) {
+    if (IS_RAW_DATA(d) && !d->ref.isShared() && size < d->size) {
         d->size = size;
         return;
     }
@@ -1334,14 +1336,14 @@ void QString::resize(int size)
 // ### Qt 5: rename reallocData() to avoid confusion. 197625
 void QString::realloc(int alloc)
 {
-    if (d->ref.isShared() || d->offset) {
+    if (d->ref.isShared() || IS_RAW_DATA(d)) {
         Data *x = static_cast<Data *>(::malloc(sizeof(Data) + (alloc+1) * sizeof(QChar)));
         Q_CHECK_PTR(x);
         x->ref.initializeOwned();
         x->size = qMin(alloc, d->size);
         x->alloc = (uint) alloc;
         x->capacityReserved = d->capacityReserved;
-        x->offset =0;
+        x->offset = sizeof(QStringData);
         ::memcpy(x->data(), d->data(), x->size * sizeof(QChar));
         x->data()[x->size] = 0;
         if (!d->ref.deref())
@@ -1359,7 +1361,7 @@ void QString::realloc(int alloc)
         Q_CHECK_PTR(p);
         d = p;
         d->alloc = alloc;
-        d->offset = 0;
+        d->offset = sizeof(QStringData);
     }
 }
 
@@ -3809,7 +3811,7 @@ QString::Data *QString::fromLatin1_helper(const char *str, int size)
         d->size = size;
         d->alloc = (uint) size;
         d->capacityReserved = false;
-        d->offset = 0;
+        d->offset = sizeof(QStringData);
         d->data()[size] = '\0';
         ushort *dst = d->data();
         /* SIMD:
@@ -4883,7 +4885,7 @@ int QString::localeAwareCompare_helper(const QChar *data1, int length1,
 
 const ushort *QString::utf16() const
 {
-    if (d->offset)
+    if (IS_RAW_DATA(d))
         const_cast<QString*>(this)->realloc();   // ensure '\\0'-termination for ::fromRawData strings
     return d->data();
 }
@@ -7147,7 +7149,7 @@ QString QString::fromRawData(const QChar *unicode, int size)
         x->size = size;
         x->alloc = 0;
         x->capacityReserved = false;
-        x->offset = (const ushort *)unicode - (x->d + sizeof(qptrdiff)/sizeof(ushort));
+        x->offset = reinterpret_cast<const char *>(unicode) - reinterpret_cast<char *>(x);
     }
     return QString(x, 0);
 }
@@ -7180,9 +7182,9 @@ QString &QString::setRawData(const QChar *unicode, int size)
 #endif
         if (unicode) {
             d->size = size;
-            d->offset = (const ushort *)unicode - (d->d + sizeof(qptrdiff)/sizeof(ushort));
+            d->offset = reinterpret_cast<const char *>(unicode) - reinterpret_cast<char *>(d);
         } else {
-            d->offset = 0;
+            d->offset = sizeof(QStringData);
             d->size = 0;
         }
     }

src/corelib/tools/qstring.h
@@ -85,15 +85,14 @@ struct QStringData {
     int size;
     uint alloc : 31;
     uint capacityReserved : 1;
+    qptrdiff offset;
+
 #ifdef QT3_SUPPORT
     bool asciiCache;
 #endif
-    union {
-        qptrdiff offset; // will always work as we add/subtract from a ushort ptr
-        ushort d[sizeof(qptrdiff)/sizeof(ushort)];
-    };
-    inline ushort *data() { return d + sizeof(qptrdiff)/sizeof(ushort) + offset; }
-    inline const ushort *data() const { return d + sizeof(qptrdiff)/sizeof(ushort) + offset; }
+
+    inline ushort *data() { return reinterpret_cast<ushort *>(reinterpret_cast<char *>(this) + offset); } 
+    inline const ushort *data() const { return reinterpret_cast<const ushort *>(reinterpret_cast<const char *>(this) + offset); }
 };
 
 
@@ -106,7 +105,7 @@ template<int N> struct QStaticStringData
     operator const QStringData &() const { return str; }
 };
 #define QStringLiteral(str) (const QStaticStringData<sizeof(u"" str)/2>) \
-{ { Q_REFCOUNT_INITIALIZE_STATIC, sizeof(u"" str)/2, 0, 0, { 0 } }, u"" str }
+{ { Q_REFCOUNT_INITIALIZE_STATIC, sizeof(u"" str)/2, 0, 0, sizeof(QStringData) }, u"" str }
 
 // wchar_t is 2 bytes
 #elif defined(Q_OS_WIN) || (defined(__SIZEOF_WCHAR_T__) && __SIZEOF_WCHAR_T__ == 2) || defined(WCHAR_MAX) && (WCHAR_MAX - 0 < 65536)
@@ -118,7 +117,7 @@ template<int N> struct QStaticStringData
     operator const QStringData &() const { return str; }
 };
 #define QStringLiteral(str) (const QStaticStringData<sizeof(L"" str)/2>) \
-{ { Q_REFCOUNT_INITIALIZE_STATIC, sizeof(L"" str)/2, 0, 0, { 0 } }, L"" str }
+{ { Q_REFCOUNT_INITIALIZE_STATIC, sizeof(L"" str)/2, 0, 0, sizeof(QStringData) }, L"" str }
 
 // fallback, uses QLatin1String as next best options
 #else
@@ -769,7 +768,7 @@ inline QChar *QString::data()
 inline const QChar *QString::constData() const
 { return reinterpret_cast<const QChar*>(d->data()); }
 inline void QString::detach()
-{ if (d->ref.isShared() || d->offset) realloc(); }
+{ if (d->ref.isShared() || (d->offset != sizeof(QStringData))) realloc(); }
 inline bool QString::isDetached() const
 { return !d->ref.isShared(); }
 inline QString &QString::operator=(const QLatin1String &s)

tests/auto/qbytearray/tst_qbytearray.cpp
@@ -1540,7 +1540,7 @@ void tst_QByteArray::literals()
     QVERIFY(str.length() == 4);
     QVERIFY(str == "abcd");
     QVERIFY(str.data_ptr()->ref == -1);
-    QVERIFY(str.data_ptr()->offset == 0);
+    QVERIFY(str.data_ptr()->offset == sizeof(QByteArrayData));
 
     const char *s = str.constData();
     QByteArray str2 = str;

commit 7c482c2b8cc6cb1b310d492c1c13ce78fd1cbe8f

Fix bugs introduced in 168bb2b

src/corelib/tools/qbytearray.cpp
@@ -1358,7 +1358,7 @@ QByteArray::QByteArray(int size, char ch)
     if (size <= 0) {
         d = const_cast<Data *>(&shared_null.ba);
     } else {
-        d = static_cast<Data *>(malloc(sizeof(Data)+size));
+        d = static_cast<Data *>(malloc(sizeof(Data) + size + 1));
         Q_CHECK_PTR(d);
         d->ref.initializeOwned();
         d->size = size;
@@ -1431,7 +1431,7 @@ void QByteArray::resize(int size)
         x->size = size;
         x->alloc = size;
         x->capacityReserved = false;
-        x->offset = 0;
+        x->offset = sizeof(QByteArrayData);
         x->data()[size] = '\0';
         d = x;
     } else {