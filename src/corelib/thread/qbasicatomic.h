21a9349b0d7124382962a37b87ff675d4f5d67fe
Leandro Nini <drfiemost@users.noreply.github.com>
2023-10-26 20:55:33 +0200

    Apply change from upstream
    https://github.com/qt/qtbase/commit/22ce26234ed9b5c8e35fb10f54c769db76eefbbb

--- src/corelib/thread/qbasicatomic.h
+++ src/corelib/thread/qbasicatomic.h
@@ -37,10 +37,10 @@
 **
 ****************************************************************************/
 
+#include <QtCore/qatomic.h>
+
 #ifndef QBASICATOMIC_H
 #define QBASICATOMIC_H
-
-#include <QtCore/qglobal.h>
 
 #if defined(QT_BOOTSTRAPPED)
 #  include <QtCore/qatomic_bootstrap.h>
