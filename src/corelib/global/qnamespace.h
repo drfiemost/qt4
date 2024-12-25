drfiemost/qt4

commit 3adea57bf1d014d45f2a7752e0e65556ab1e6680

Apply changes from upstream
https://github.com/qt/qtbase/commit/fee009bcf915b556eb3c9695b02ae6f23d9a23c7
https://github.com/qt/qtbase/commit/498f938f2d0a206b6864457cac9f96b179ac1216
https://github.com/qt/qtbase/commit/6d28965b10d138bf2214ac44048945964cc768a6
https://github.com/qt/qtbase/commit/dffc816f494f46e21fa53ff4936210a6abbcabff
https://github.com/qt/qtbase/commit/864a2dfa9b1e172c6c86f24f2be1e696edbbe578
https://github.com/qt/qtbase/commit/25028e3b144a54435be437d452b65bfbd4f63309
https://github.com/qt/qtbase/commit/fbef41167aa4aede67bc1ec904142e2e5e12c7fc

src/corelib/global/qnamespace.h
@@ -276,13 +276,13 @@ public:
         Window = 0x00000001,
         Dialog = 0x00000002 | Window,
         Sheet = 0x00000004 | Window,
-        Drawer = 0x00000006 | Window,
+        Drawer = Sheet | Dialog,
         Popup = 0x00000008 | Window,
-        Tool = 0x0000000a | Window,
-        ToolTip = 0x0000000c | Window,
-        SplashScreen = 0x0000000e | Window,
+        Tool = Popup | Dialog,
+        ToolTip = Popup | Sheet,
+        SplashScreen = ToolTip | Dialog,
         Desktop = 0x00000010 | Window,
-        SubWindow =  0x00000012,
+        SubWindow = Desktop | Dialog,
 
         WindowType_Mask = 0x000000ff,
         MSWindowsFixedSizeDialogHint = 0x00000100,

src/gui/kernel/kernel.pri
@@ -180,7 +180,7 @@ embedded {
 
 qpa {
 	HEADERS += \
-		kernel/qgenericpluginfactory_qpa.h \
+                kernel/qgenericpluginfactory_qpa.h \
                 kernel/qgenericplugin_qpa.h \
                 kernel/qeventdispatcher_qpa_p.h \
                 kernel/qwindowsysteminterface_qpa.h \
@@ -190,23 +190,24 @@ qpa {
                 kernel/qplatformintegrationfactory_qpa_p.h \
                 kernel/qplatformintegrationplugin_qpa.h \
                 kernel/qplatformwindow_qpa.h \
-                kernel/qplatformwindowformat_qpa.h \
                 kernel/qplatformglcontext_qpa.h \
                 kernel/qdesktopwidget_qpa_p.h \
                 kernel/qplatformeventloopintegration_qpa.h \
                 kernel/qplatformcursor_qpa.h \
                 kernel/qplatformclipboard_qpa.h \
-                kernel/qplatformnativeinterface_qpa.h
+                kernel/qplatformnativeinterface_qpa.h \
+                kernel/qwindowformat_qpa.h \
+                kernel/qwindow_qpa.h
 
 	SOURCES += \
-		kernel/qapplication_qpa.cpp \
-		kernel/qclipboard_qpa.cpp \
-		kernel/qcursor_qpa.cpp \
-		kernel/qdnd_qws.cpp \
-		kernel/qdesktopwidget_qpa.cpp \
-		kernel/qgenericpluginfactory_qpa.cpp \
-		kernel/qgenericplugin_qpa.cpp \
-		kernel/qkeymapper_qws.cpp \
+                kernel/qapplication_qpa.cpp \
+                kernel/qclipboard_qpa.cpp \
+                kernel/qcursor_qpa.cpp \
+                kernel/qdnd_qws.cpp \
+                kernel/qdesktopwidget_qpa.cpp \
+                kernel/qgenericpluginfactory_qpa.cpp \
+                kernel/qgenericplugin_qpa.cpp \
+                kernel/qkeymapper_qws.cpp \
                 kernel/qwidget_qpa.cpp \
                 kernel/qeventdispatcher_qpa.cpp \
                 kernel/qwindowsysteminterface_qpa.cpp \
@@ -215,13 +216,14 @@ qpa {
                 kernel/qplatformintegrationfactory_qpa.cpp \
                 kernel/qplatformintegrationplugin_qpa.cpp \
                 kernel/qplatformwindow_qpa.cpp \
-                kernel/qplatformwindowformat_qpa.cpp \
                 kernel/qplatformeventloopintegration_qpa.cpp \
                 kernel/qplatformglcontext_qpa.cpp \
                 kernel/qplatformcursor_qpa.cpp \
                 kernel/qplatformclipboard_qpa.cpp \
                 kernel/qplatformnativeinterface_qpa.cpp \
-                kernel/qsessionmanager_qpa.cpp
+                kernel/qsessionmanager_qpa.cpp \
+                kernel/qwindowformat_qpa.cpp \
+                kernel/qwindow_qpa.cpp
 
         contains(QT_CONFIG, glib) {
             SOURCES += \

src/gui/kernel/qplatformglcontext_qpa.h
@@ -43,7 +43,7 @@
 #define QPLATFORM_GL_CONTEXT_H
 
 #include <QtCore/qnamespace.h>
-#include <QtGui/QPlatformWindowFormat>
+#include <QtGui/QWindowFormat>
 
 QT_BEGIN_HEADER
 
@@ -66,7 +66,7 @@ public:
     virtual void swapBuffers() = 0;
     virtual void* getProcAddress(const QString& procName) = 0;
 
-    virtual QPlatformWindowFormat platformWindowFormat() const = 0;
+    virtual QWindowFormat windowFormat() const = 0;
 
     const static QPlatformGLContext *currentContext();
 

src/gui/kernel/qplatformintegration_qpa.h
@@ -54,13 +54,14 @@ QT_BEGIN_NAMESPACE
 QT_MODULE(Gui)
 
 class QPlatformWindow;
-class QWindowSurface;
+class QWindow;
 class QBlittable;
 class QWidget;
 class QPlatformEventLoopIntegration;
 class QPlatformFontDatabase;
 class QPlatformClipboard;
 class QPlatformNativeInterface;
+
 
 class Q_GUI_EXPORT QPlatformIntegration
 {
@@ -76,8 +77,8 @@ public:
 
 // GraphicsSystem functions
     virtual QPixmapData *createPixmapData(QPixmapData::PixelType type) const = 0;
-    virtual QPlatformWindow *createPlatformWindow(QWidget *widget, WId winId = 0) const = 0;
-    virtual QWindowSurface *createWindowSurface(QWidget *widget, WId winId) const = 0;
+    virtual QPlatformWindow *createPlatformWindow(QWindow *window, const QWindowFormat &format) const = 0;
+    virtual QWindowSurface *createWindowSurface(QWindow *window, WId winId) const = 0;
 
 // Window System functions
     virtual QList<QPlatformScreen *> screens() const = 0;

src/gui/kernel/qplatformwindow_qpa.cpp
@@ -46,7 +46,7 @@
 
 class QPlatformWindowPrivate
 {
-    QWidget *tlw;
+    QWindow *window;
     QRect rect;
     Qt::WindowFlags flags;
     friend class QPlatformWindow;
@@ -56,12 +56,11 @@ class QPlatformWindowPrivate
     Constructs a platform window with the given top level widget.
 */
 
-QPlatformWindow::QPlatformWindow(QWidget *tlw)
+QPlatformWindow::QPlatformWindow(QWindow *window)
     : d_ptr(new QPlatformWindowPrivate)
 {
     Q_D(QPlatformWindow);
-    d->tlw = tlw;
-    tlw->setPlatformWindow(this);
+    d->window = window;
 }
 
 /*!
@@ -74,10 +73,10 @@ QPlatformWindow::~QPlatformWindow()
 /*!
     Returnes the widget which belongs to the QPlatformWindow
 */
-QWidget *QPlatformWindow::widget() const
+QWindow *QPlatformWindow::window() const
 {
     Q_D(const QPlatformWindow);
-    return d->tlw;
+    return d->window;
 }
 
 /*!
@@ -186,7 +185,7 @@ void QPlatformWindow::setOpacity(qreal level)
 */
 void QPlatformWindow::requestActivateWindow()
 {
-    QWindowSystemInterface::handleWindowActivated(widget());
+    QWindowSystemInterface::handleWindowActivated(window()->widget());
 }
 
 /*!

src/gui/kernel/qplatformwindow_qpa.h
@@ -55,17 +55,17 @@ QT_BEGIN_NAMESPACE
 QT_MODULE(Gui)
 
 class QPlatformWindowPrivate;
-class QWidget;
+class QWindow;
 class QPlatformGLContext;
 
 class Q_GUI_EXPORT QPlatformWindow
 {
     Q_DECLARE_PRIVATE(QPlatformWindow)
 public:
-    QPlatformWindow(QWidget *tlw);
+    QPlatformWindow(QWindow *window);
     virtual ~QPlatformWindow();
 
-    QWidget *widget() const;
+    QWindow *window() const;
     virtual void setGeometry(const QRect &rect);
     virtual QRect geometry() const;
 

src/gui/kernel/qplatformwindowformat_qpa.h
@@ -1,234 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the QtGui module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see http://www.qt.io/terms-conditions. For further
-** information use the contact form at http://www.qt.io/contact-us.
-**
-** GNU Lesser General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** As a special exception, The Qt Company gives you certain additional
-** rights. These rights are described in The Qt Company LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-#ifndef QPLATFORMWINDOWFORMAT_QPA_H
-#define QPLATFORMWINDOWFORMAT_QPA_H
-
-#include <QtGui/QPlatformWindow>
-
-QT_BEGIN_HEADER
-
-QT_BEGIN_NAMESPACE
-
-QT_MODULE(Gui)
-
-class QPlatformWindowFormatPrivate;
-
-class Q_GUI_EXPORT QPlatformWindowFormat
-{
-public:
-    enum FormatOption {
-        DoubleBuffer            = 0x0001,
-        DepthBuffer             = 0x0002,
-        Rgba                    = 0x0004,
-        AlphaChannel            = 0x0008,
-        AccumBuffer             = 0x0010,
-        StencilBuffer           = 0x0020,
-        StereoBuffers           = 0x0040,
-        DirectRendering         = 0x0080,
-        HasOverlay              = 0x0100,
-        SampleBuffers           = 0x0200,
-        DeprecatedFunctions     = 0x0400,
-        HasWindowSurface        = 0x0800,
-        SingleBuffer            = DoubleBuffer    << 16,
-        NoDepthBuffer           = DepthBuffer     << 16,
-        ColorIndex              = Rgba            << 16,
-        NoAlphaChannel          = AlphaChannel    << 16,
-        NoAccumBuffer           = AccumBuffer     << 16,
-        NoStencilBuffer         = StencilBuffer   << 16,
-        NoStereoBuffers         = StereoBuffers   << 16,
-        IndirectRendering       = DirectRendering << 16,
-        NoOverlay               = HasOverlay      << 16,
-        NoSampleBuffers         = SampleBuffers   << 16,
-        NoDeprecatedFunctions   = DeprecatedFunctions << 16,
-        NoWindowSurface         = HasWindowSurface << 16
-
-    };
-    Q_DECLARE_FLAGS(FormatOptions, FormatOption)
-
-    enum WindowApi {
-        Raster,
-        OpenGL,
-        OpenVG
-    };
-
-    QPlatformWindowFormat();
-    QPlatformWindowFormat(FormatOptions options);
-    QPlatformWindowFormat(const QPlatformWindowFormat &other);
-    QPlatformWindowFormat &operator=(const QPlatformWindowFormat &other);
-    ~QPlatformWindowFormat();
-
-    void setDepthBufferSize(int size);
-    int  depthBufferSize() const;
-
-    void setAccumBufferSize(int size);
-    int  accumBufferSize() const;
-
-    void setRedBufferSize(int size);
-    int  redBufferSize() const;
-
-    void setGreenBufferSize(int size);
-    int  greenBufferSize() const;
-
-    void setBlueBufferSize(int size);
-    int  blueBufferSize() const;
-
-    void setAlphaBufferSize(int size);
-    int  alphaBufferSize() const;
-
-    void setStencilBufferSize(int size);
-    int  stencilBufferSize() const;
-
-    void setSampleBuffers(bool enable);
-    bool sampleBuffers() const;
-
-    void setSamples(int numSamples);
-    int  samples() const;
-
-    void setSwapInterval(int interval);
-    int  swapInterval() const;
-
-    void setWindowApi(QPlatformWindowFormat::WindowApi api);
-    WindowApi windowApi() const;
-
-    void setSharedContext(QPlatformGLContext *context);
-    QPlatformGLContext *sharedGLContext() const;
-
-    bool doubleBuffer() const;
-    void setDoubleBuffer(bool enable);
-    bool depth() const;
-    void setDepth(bool enable);
-    bool rgba() const;
-    void setRgba(bool enable);
-    bool alpha() const;
-    void setAlpha(bool enable);
-    bool accum() const;
-    void setAccum(bool enable);
-    bool stencil() const;
-    void setStencil(bool enable);
-    bool stereo() const;
-    void setStereo(bool enable);
-    bool directRendering() const;
-    void setDirectRendering(bool enable);
-    bool hasWindowSurface() const;
-    void setWindowSurface(bool enable);
-
-    void setOption(QPlatformWindowFormat::FormatOptions opt);
-    bool testOption(QPlatformWindowFormat::FormatOptions opt) const;
-
-    static QPlatformWindowFormat defaultFormat();
-    static void setDefaultFormat(const QPlatformWindowFormat& f);
-
-private:
-    QPlatformWindowFormatPrivate *d;
-
-    void detach();
-
-    friend Q_GUI_EXPORT bool operator==(const QPlatformWindowFormat&, const QPlatformWindowFormat&);
-    friend Q_GUI_EXPORT bool operator!=(const QPlatformWindowFormat&, const QPlatformWindowFormat&);
-#ifndef QT_NO_DEBUG_STREAM
-    friend Q_GUI_EXPORT QDebug operator<<(QDebug, const QPlatformWindowFormat &);
-#endif
-};
-
-Q_GUI_EXPORT bool operator==(const QPlatformWindowFormat&, const QPlatformWindowFormat&);
-Q_GUI_EXPORT bool operator!=(const QPlatformWindowFormat&, const QPlatformWindowFormat&);
-
-#ifndef QT_NO_DEBUG_STREAM
-Q_OPENGL_EXPORT QDebug operator<<(QDebug, const QPlatformWindowFormat &);
-#endif
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(QPlatformWindowFormat::FormatOptions)
-
-inline bool QPlatformWindowFormat::doubleBuffer() const
-{
-    return testOption(QPlatformWindowFormat::DoubleBuffer);
-}
-
-inline bool QPlatformWindowFormat::depth() const
-{
-    return testOption(QPlatformWindowFormat::DepthBuffer);
-}
-
-inline bool QPlatformWindowFormat::rgba() const
-{
-    return testOption(QPlatformWindowFormat::Rgba);
-}
-
-inline bool QPlatformWindowFormat::alpha() const
-{
-    return testOption(QPlatformWindowFormat::AlphaChannel);
-}
-
-inline bool QPlatformWindowFormat::accum() const
-{
-    return testOption(QPlatformWindowFormat::AccumBuffer);
-}
-
-inline bool QPlatformWindowFormat::stencil() const
-{
-    return testOption(QPlatformWindowFormat::StencilBuffer);
-}
-
-inline bool QPlatformWindowFormat::stereo() const
-{
-    return testOption(QPlatformWindowFormat::StereoBuffers);
-}
-
-inline bool QPlatformWindowFormat::directRendering() const
-{
-    return testOption(QPlatformWindowFormat::DirectRendering);
-}
-
-inline bool QPlatformWindowFormat::hasWindowSurface() const
-{
-    return testOption(QPlatformWindowFormat::HasWindowSurface);
-}
-
-inline bool QPlatformWindowFormat::sampleBuffers() const
-{
-    return testOption(QPlatformWindowFormat::SampleBuffers);
-}
-
-QT_END_NAMESPACE
-
-QT_END_HEADER
-
-#endif //QPLATFORMWINDOWFORMAT_QPA_H

commit c1f05fd654dab305935d8e94359580dcafa78723

Fix longstanding regression
for MDI windows introduced with 3adea57bf1d014d45f2a7752e0e65556ab1e6680

src/corelib/global/qnamespace.h
@@ -252,7 +252,7 @@ public:
         ToolTip = Popup | Sheet,
         SplashScreen = ToolTip | Dialog,
         Desktop = 0x00000010 | Window,
-        SubWindow = Desktop | Dialog,
+        SubWindow = 0x00000012,
 
         WindowType_Mask = 0x000000ff,
         MSWindowsFixedSizeDialogHint = 0x00000100,