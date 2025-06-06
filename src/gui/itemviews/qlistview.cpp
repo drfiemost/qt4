drfiemost/qt4

commit 792623df2bfc358a13ac3fab8f11654eee9dc718

Remove deprecated functions and deprecate more

src/gui/itemviews/qlistview.cpp
@@ -684,7 +684,7 @@ void QListView::reset()
 void QListView::setRootIndex(const QModelIndex &index)
 {
     Q_D(QListView);
-    d->column = qBound(0, d->column, d->model->columnCount(index) - 1);
+    d->column = std::clamp(d->column, 0, d->model->columnCount(index) - 1);
     QAbstractItemView::setRootIndex(index);
     // sometimes we get an update before reset() is called
     d->clear();
@@ -2177,7 +2177,7 @@ int QListModeViewBase::verticalScrollToValue(int index, QListView::ScrollHint hi
             for (int i = 0; i < flowPositions.count() - 1 && i <= scrollBarValue; ++i)
                 if (isHidden(i))
                     ++numHidden;
-            value = qBound(0, scrollValueMap.at(verticalScrollBar()->value()) - numHidden, flowPositions.count() - 1);
+            value = std::clamp(scrollValueMap.at(verticalScrollBar()->value()) - numHidden, 0, flowPositions.count() - 1);
         }
         if (above)
             hint = QListView::PositionAtTop;
@@ -2198,9 +2198,9 @@ int QListModeViewBase::horizontalOffset() const
         if (isWrapping()) {
             if (flow() == QListView::TopToBottom && !segmentPositions.isEmpty()) {
                 const int max = segmentPositions.count() - 1;
-                int currentValue = qBound(0, horizontalScrollBar()->value(), max);
+                int currentValue = std::clamp(horizontalScrollBar()->value(), 0, max);
                 int position = segmentPositions.at(currentValue);
-                int maximumValue = qBound(0, horizontalScrollBar()->maximum(), max);
+                int maximumValue = std::clamp(horizontalScrollBar()->maximum(), 0, max);
                 int maximum = segmentPositions.at(maximumValue);
                 return (isRightToLeft() ? maximum - position : position);
             }
@@ -2243,7 +2243,7 @@ int QListModeViewBase::horizontalScrollToValue(int index, QListView::ScrollHint 
     if (scrollValueMap.isEmpty())
         value = 0;
     else
-        value = qBound(0, scrollValueMap.at(horizontalScrollBar()->value()), flowPositions.count() - 1);
+        value = std::clamp(scrollValueMap.at(horizontalScrollBar()->value()), 0, flowPositions.count() - 1);
     if (leftOf)
         hint = QListView::PositionAtTop;
     else if (rightOf)
@@ -2267,14 +2267,14 @@ void QListModeViewBase::scrollContentsBy(int dx, int dy, bool scrollElasticBand)
             return;
         const int max = segmentPositions.count() - 1;
         if (horizontal && flow() == QListView::TopToBottom && dx != 0) {
-            int currentValue = qBound(0, horizontalValue, max);
-            int previousValue = qBound(0, currentValue + dx, max);
+            int currentValue = std::clamp(horizontalValue, 0, max);
+            int previousValue = std::clamp(currentValue + dx, 0, max);
             int currentCoordinate = segmentPositions.at(currentValue) - spacing();
             int previousCoordinate = segmentPositions.at(previousValue) - spacing();
             dx = previousCoordinate - currentCoordinate;
         } else if (vertical && flow() == QListView::LeftToRight && dy != 0) {
-            int currentValue = qBound(0, verticalValue, max);
-            int previousValue = qBound(0, currentValue + dy, max);
+            int currentValue = std::clamp(verticalValue, 0, max);
+            int previousValue = std::clamp(currentValue + dy, 0, max);
             int currentCoordinate = segmentPositions.at(currentValue) - spacing();
             int previousCoordinate = segmentPositions.at(previousValue) - spacing();
             dy = previousCoordinate - currentCoordinate;
@@ -2284,14 +2284,14 @@ void QListModeViewBase::scrollContentsBy(int dx, int dy, bool scrollElasticBand)
             return;
         const int max = scrollValueMap.count() - 1;
         if (vertical && flow() == QListView::TopToBottom && dy != 0) {
-            int currentValue = qBound(0, verticalValue, max);
-            int previousValue = qBound(0, currentValue + dy, max);
+            int currentValue = std::clamp(verticalValue, 0, max);
+            int previousValue = std::clamp(currentValue + dy, 0, max);
             int currentCoordinate = flowPositions.at(scrollValueMap.at(currentValue));
             int previousCoordinate = flowPositions.at(scrollValueMap.at(previousValue));
             dy = previousCoordinate - currentCoordinate;
         } else if (horizontal && flow() == QListView::LeftToRight && dx != 0) {
-            int currentValue = qBound(0, horizontalValue, max);
-            int previousValue = qBound(0, currentValue + dx, max);
+            int currentValue = std::clamp(horizontalValue, 0, max);
+            int previousValue = std::clamp(currentValue + dx, 0, max);
             int currentCoordinate = flowPositions.at(scrollValueMap.at(currentValue));
             int previousCoordinate = flowPositions.at(scrollValueMap.at(previousValue));
             dx = previousCoordinate - currentCoordinate;

src/corelib/global/qglobal.h
@@ -1262,10 +1262,6 @@ typedef double qreal;
    Utility macros and inline functions
 */
 
-template <typename T>
-[[deprecated("Use std::abs")]]
-Q_DECL_CONSTEXPR inline T qAbs(const T &t) { return t >= 0 ? t : -t; }
-
 Q_DECL_CONSTEXPR inline int qRound(qreal d)
 { return d >= qreal(0.0) ? int(d + qreal(0.5)) : int(d - int(d-1) + qreal(0.5)) + int(d-1); }
 
@@ -1278,13 +1274,7 @@ Q_DECL_CONSTEXPR inline qint64 qRound64(qreal d)
 #endif
 
 template <typename T>
-[[deprecated("Use std::min")]]
-Q_DECL_CONSTEXPR inline const T &qMin(const T &a, const T &b) { return (a < b) ? a : b; }
-template <typename T>
-[[deprecated("Use std::max")]]
-Q_DECL_CONSTEXPR inline const T &qMax(const T &a, const T &b) { return (a < b) ? b : a; }
-template <typename T>
-//[[deprecated("Use std::clamp")]]
+[[deprecated("Use std::clamp")]]
 Q_DECL_CONSTEXPR inline const T &qBound(const T &min, const T &val, const T &max)
 { return std::max(min, std::min(max, val)); }
 

src/corelib/kernel/qabstractitemmodel.cpp
@@ -2358,7 +2358,7 @@ bool QAbstractItemModel::decodeData(int row, int column, const QModelIndex &pare
         int flat = (relativeRow * dragColumnCount) + relativeColumn;
         // if the item was already written to, or we just can't fit it in the table, create a new row
         if (destinationColumn >= colCount || isWrittenTo.testBit(flat)) {
-            destinationColumn = qBound(column, destinationColumn, colCount - 1);
+            destinationColumn = std::clamp(destinationColumn, column, colCount - 1);
             destinationRow = row + dragRowCount;
             insertRows(row + dragRowCount, 1, parent);
             flat = (dragRowCount * dragColumnCount) + relativeColumn;

src/corelib/kernel/qtimerinfo_unix.cpp
@@ -89,7 +89,6 @@ timespec QTimerInfoList::updateCurrentTime()
 
 #if ((_POSIX_MONOTONIC_CLOCK-0 <= 0) && !defined(Q_OS_MAC)) || defined(QT_BOOTSTRAPPED)
 
-template <>
 timespec qAbs(const timespec &t)
 {
     timespec tmp = t;

src/corelib/tools/qtextboundaryfinder.cpp
@@ -316,7 +316,7 @@ int QTextBoundaryFinder::position() const
 */
 void QTextBoundaryFinder::setPosition(int position)
 {
-    pos = qBound(0, position, length);
+    pos = std::clamp(position, 0, length);
 }
 
 /*! \fn QTextBoundaryFinder::BoundaryType QTextBoundaryFinder::type() const

src/dbus/qdbuserror.cpp
@@ -140,7 +140,7 @@ static const int errorMessages_count = sizeof errorMessages_indices /
 
 static inline const char *get(QDBusError::ErrorType code)
 {
-    int intcode = qBound(0, int(code) - int(QDBusError::Other), errorMessages_count);
+    int intcode = std::clamp(int(code) - int(QDBusError::Other), 0, errorMessages_count);
     return errorMessages_string + errorMessages_indices[intcode];
 }
 

src/gui/animation/qguivariantanimation.cpp
@@ -53,10 +53,10 @@ QT_BEGIN_NAMESPACE
 
 template<> Q_INLINE_TEMPLATE QColor _q_interpolate(const QColor &f,const QColor &t, qreal progress)
 {
-    return QColor(qBound(0,_q_interpolate(f.red(), t.red(), progress),255),
-                  qBound(0,_q_interpolate(f.green(), t.green(), progress),255),
-                  qBound(0,_q_interpolate(f.blue(), t.blue(), progress),255),
-                  qBound(0,_q_interpolate(f.alpha(), t.alpha(), progress),255));
+    return QColor(std::clamp(_q_interpolate(f.red(), t.red(), progress),0,255),
+                  std::clamp(_q_interpolate(f.green(), t.green(), progress),0,255),
+                  std::clamp(_q_interpolate(f.blue(), t.blue(), progress),0,255),
+                  std::clamp(_q_interpolate(f.alpha(), t.alpha(), progress),0,255));
 }
 
 template<> Q_INLINE_TEMPLATE QQuaternion _q_interpolate(const QQuaternion &f,const QQuaternion &t, qreal progress)

src/gui/effects/qgraphicseffect.cpp
@@ -1116,7 +1116,7 @@ qreal QGraphicsOpacityEffect::opacity() const
 void QGraphicsOpacityEffect::setOpacity(qreal opacity)
 {
     Q_D(QGraphicsOpacityEffect);
-    opacity = qBound(qreal(0.0), opacity, qreal(1.0));
+    opacity = std::clamp(opacity, qreal(0.0), qreal(1.0));
 
     if (qFuzzyCompare(d->opacity, opacity))
         return;

commit 2fc20e6b7626acd6d0e5d3a4d264a3cfae58bf7e

Fix regression introduced in 792623d

src/gui/itemviews/qlistview.cpp
@@ -684,7 +684,7 @@ void QListView::reset()
 void QListView::setRootIndex(const QModelIndex &index)
 {
     Q_D(QListView);
-    d->column = std::clamp(d->column, 0, d->model->columnCount(index) - 1);
+    d->column = std::clamp(d->column, 0, std::max(0, d->model->columnCount(index) - 1));
     QAbstractItemView::setRootIndex(index);
     // sometimes we get an update before reset() is called
     d->clear();
@@ -2176,7 +2176,7 @@ int QListModeViewBase::verticalScrollToValue(int index, QListView::ScrollHint hi
             for (int i = 0; i < flowPositions.count() - 1 && i <= scrollBarValue; ++i)
                 if (isHidden(i))
                     ++numHidden;
-            value = std::clamp(scrollValueMap.at(verticalScrollBar()->value()) - numHidden, 0, flowPositions.count() - 1);
+            value = std::clamp(scrollValueMap.at(verticalScrollBar()->value()) - numHidden, 0, std::max(0, flowPositions.count() - 1));
         }
         if (above)
             hint = QListView::PositionAtTop;
@@ -2242,7 +2242,7 @@ int QListModeViewBase::horizontalScrollToValue(int index, QListView::ScrollHint 
     if (scrollValueMap.isEmpty())
         value = 0;
     else
-        value = std::clamp(scrollValueMap.at(horizontalScrollBar()->value()), 0, flowPositions.count() - 1);
+        value = std::clamp(scrollValueMap.at(horizontalScrollBar()->value()), 0, std::max(0, flowPositions.count() - 1));
     if (leftOf)
         hint = QListView::PositionAtTop;
     else if (rightOf)