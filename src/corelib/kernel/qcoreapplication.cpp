drfiemost/qt4

commit 17419f6f8ee8d21b6b7d905623ada78c22c82364

Apply changes from upstream
https://github.com/qt/qtbase/commit/07f6eff58d5ffd8d148cd848141be1ba11c6c5da
https://github.com/qt/qtbase/commit/a3d50112e44bc42b310d9d3a8e6c7805ef31ef53
https://github.com/qt/qtbase/commit/9bac90db46913ae70fd29676cb22890b4ac9aaa4

src/corelib/kernel/qcoreapplication.cpp
@@ -342,7 +342,8 @@ void QCoreApplicationPrivate::createEventDispatcher()
 #endif
 }
 
-QBasicAtomicPointer<QThread> QCoreApplicationPrivate::theMainThread = Q_BASIC_ATOMIC_INITIALIZER(0);
+QBasicAtomicPointer<QThread> QCoreApplicationPrivate::theMainThread = Q_BASIC_ATOMIC_INITIALIZER(nullptr);
+QBasicAtomicPointer<void> QCoreApplicationPrivate::theMainThreadId = Q_BASIC_ATOMIC_INITIALIZER(nullptr);
 QThread *QCoreApplicationPrivate::mainThread()
 {
     Q_ASSERT(theMainThread.loadRelaxed() != 0);
@@ -773,13 +774,15 @@ bool QCoreApplication::notify(QObject *receiver, QEvent *event)
 
 bool QCoreApplicationPrivate::sendThroughApplicationEventFilters(QObject *receiver, QEvent *event)
 {
-    auto thisThreadData = threadData.loadRelaxed();
-    if ((receiver->d_func()->threadData.loadRelaxed() == thisThreadData) && extraData) {
+    // We can't access the application event filters outside of the main thread (race conditions)
+    Q_ASSERT(QThread::isMainThread());
+
+    if (extraData) {
         // application event filters are only called for objects in the GUI thread
         for (const auto & obj : extraData->eventFilters) {
             if (!obj)
                 continue;
-            if (obj->d_func()->threadData.loadRelaxed() != thisThreadData) {
+            if (obj->d_func()->threadData.loadRelaxed() != receiver->d_func()->threadData.loadRelaxed()) {
                 qWarning("QCoreApplication: Application event filter cannot be in a different thread.");
                 continue;
             }

src/corelib/kernel/qcoreapplication_p.h
@@ -91,6 +91,7 @@ public:
 #endif
 
     static QBasicAtomicPointer<QThread> theMainThread;
+    static QBasicAtomicPointer<void> theMainThreadId;
     static QThread *mainThread();
     static bool checkInstance(const char *method);
     static void sendPostedEvents(QObject *receiver, int event_type, QThreadData *data);

src/corelib/thread/qthread.cpp
@@ -92,8 +92,9 @@ QThreadData::~QThreadData()
     // safeguard the main thread here.. This fix is a bit crude, but it solves
     // the problem...
     if (this->thread.loadAcquire() == QCoreApplicationPrivate::theMainThread.loadAcquire()) {
-       QCoreApplicationPrivate::theMainThread.storeRelease(nullptr);
-       QThreadData::clearCurrentThreadData();
+        QCoreApplicationPrivate::theMainThread.storeRelease(nullptr);
+        QCoreApplicationPrivate::theMainThreadId.storeRelaxed(nullptr);
+        QThreadData::clearCurrentThreadData();
     }
 
     QThread *t = thread.loadAcquire();
@@ -390,6 +391,19 @@ QThread *QThread::currentThread()
     QThreadData *data = QThreadData::current();
     Q_ASSERT(data != nullptr);
     return data->thread.loadAcquire();
+}
+
+/*!
+    Returns whether the currently executing thread is the main thread.
+    The main thread is the thread in which QCoreApplication was created.
+    This is usually the thread that called the \c{main()} function, but not necessarily so.
+    It is the thread that is processing the GUI events and in which graphical objects
+    (QWindow, QWidget) can be created.
+    \sa currentThread(), QCoreApplication::instance()
+*/
+bool QThread::isMainThread()
+{
+    return currentThreadId() == QCoreApplicationPrivate::theMainThreadId.loadRelaxed();
 }
 
 /*!
@@ -783,9 +797,13 @@ QThreadData *QThreadData::current(bool createIfNecessary)
     if (!data && createIfNecessary) {
         data = new QThreadData;
         data->thread = new QAdoptedThread(data);
+        data->threadId.storeRelaxed(Qt::HANDLE(data->thread.loadAcquire()));
         data->deref();
-        if (!QCoreApplicationPrivate::theMainThread)
-            QCoreApplicationPrivate::theMainThread = data->thread.load();
+        data->isAdopted = true;
+        if (!QCoreApplicationPrivate::theMainThread.loadAcquire()) {
+            QCoreApplicationPrivate::theMainThread.storeRelease(data->thread.loadRelaxed());
+            QCoreApplicationPrivate::theMainThreadId.storeRelaxed(data->threadId.loadRelaxed());
+        }
     }
     return data;
 }

src/corelib/thread/qthread.h
@@ -61,6 +61,7 @@ class Q_CORE_EXPORT QThread : public QObject
 public:
     static void* currentThreadId();
     static QThread *currentThread();
+    static bool isMainThread();
     static int idealThreadCount();
     static void yieldCurrentThread();
 

src/corelib/thread/qthread_unix.cpp
@@ -224,8 +224,10 @@ QThreadData *QThreadData::current()
         }
         data->isAdopted = true;
         data->threadId.storeRelaxed(to_HANDLE(pthread_self()));
-        if (!QCoreApplicationPrivate::theMainThread.loadAcquire())
+        if (!QCoreApplicationPrivate::theMainThread.loadAcquire()) {
             QCoreApplicationPrivate::theMainThread.storeRelease(data->thread.loadRelaxed());
+            QCoreApplicationPrivate::theMainThreadId.storeRelaxed(data->threadId.loadRelaxed());
+        }
     }
     return data;
 }

commit 5e586f903bed4342575e9fb5b3a62128ae01480c

Fix an error introduced with 17419f6

src/corelib/kernel/qcoreapplication.cpp
@@ -782,7 +782,7 @@ bool QCoreApplicationPrivate::sendThroughApplicationEventFilters(QObject *receiv
         for (const auto & obj : extraData->eventFilters) {
             if (!obj)
                 continue;
-            if (obj->d_func()->threadData.loadRelaxed() != receiver->d_func()->threadData.loadRelaxed()) {
+            if (obj->d_func()->threadData.loadRelaxed() != threadData.loadRelaxed()) {
                 qWarning("QCoreApplication: Application event filter cannot be in a different thread.");
                 continue;
             }